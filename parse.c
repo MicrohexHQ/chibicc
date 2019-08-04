f5b80060b292ead2defd47a7aacc094c887085dd
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-04 18:25:20 +0900

    Support argument-less function calls

--- parse.c
+++ parse.c
@@ -237,7 +237,8 @@ Node *unary() {
   return primary();
 }
 
-// primary = "(" expr ")" | ident | num
+// primary = "(" expr ")" | ident args? | num
+// args = "(" ")"
 Node *primary() {
   if (consume("(")) {
     Node *node = expr();
@@ -247,6 +248,15 @@ Node *primary() {
 
   Token *tok = consume_ident();
   if (tok) {
+    // Function call
+    if (consume("(")) {
+      expect(")");
+      Node *node = new_node(ND_FUNCALL);
+      node->funcname = strndup(tok->str, tok->len);
+      return node;
+    }
+
+    // Variable
     Var *var = find_var(tok);
     if (!var)
       var = new_lvar(strndup(tok->str, tok->len));
