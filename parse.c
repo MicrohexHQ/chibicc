43b2e366146ce6798625152d05d42ffe1dbf48e8
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-07 09:25:19 +0900

    Handle block scope

--- parse.c
+++ parse.c
@@ -4,16 +4,11 @@
 // accumulated to this list.
 VarList *locals;
 VarList *globals;
+VarList *scope;
 
-// Find a local variable by name.
+// Find a variable by name.
 Var *find_var(Token *tok) {
-  for (VarList *vl = locals; vl; vl = vl->next) {
-    Var *var = vl->var;
-    if (strlen(var->name) == tok->len && !strncmp(tok->str, var->name, tok->len))
-      return var;
-  }
-
-  for (VarList *vl = globals; vl; vl = vl->next) {
+  for (VarList *vl = scope; vl; vl = vl->next) {
     Var *var = vl->var;
     if (strlen(var->name) == tok->len && !strncmp(tok->str, var->name, tok->len))
       return var;
@@ -58,6 +53,11 @@ Var *new_var(char *name, Type *ty, bool is_local) {
   var->name = name;
   var->ty = ty;
   var->is_local = is_local;
+
+  VarList *sc = calloc(1, sizeof(VarList));
+  sc->var = var;
+  sc->next = scope;
+  scope = sc;
   return var;
 }
 
@@ -195,6 +195,8 @@ Function *function() {
   basetype();
   fn->name = expect_ident();
   expect("(");
+
+  VarList *sc = scope;
   fn->params = read_func_params();
   expect("{");
 
@@ -204,6 +206,7 @@ Function *function() {
     cur->next = stmt();
     cur = cur->next;
   }
+  scope = sc;
 
   fn->node = head.next;
   fn->locals = locals;
@@ -312,10 +315,12 @@ Node *stmt2() {
     Node head = {};
     Node *cur = &head;
 
+    VarList *sc = scope;
     while (!consume("}")) {
       cur->next = stmt();
       cur = cur->next;
     }
+    scope = sc;
 
     Node *node = new_node(ND_BLOCK, tok);
     node->body = head.next;
@@ -467,6 +472,8 @@ Node *postfix() {
 //
 // Statement expression is a GNU C extension.
 Node *stmt_expr(Token *tok) {
+  VarList *sc = scope;
+
   Node *node = new_node(ND_STMT_EXPR, tok);
   node->body = stmt();
   Node *cur = node->body;
@@ -476,6 +483,8 @@ Node *stmt_expr(Token *tok) {
     cur = cur->next;
   }
   expect(")");
+
+  scope = sc;
 
   if (cur->kind != ND_EXPR_STMT)
     error_tok(cur->tok, "stmt expr returning void is not supported");
